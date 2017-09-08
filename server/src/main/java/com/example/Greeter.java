7b47f6510430e99dcc06e059cbf90ebdafbc8793
Hayk Arutyunyan <Hayk.Arutyunyan@jpl.nasa.gov>
2017-09-08 11:03:28 -0700

    fix checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -2,6 +2,7 @@ package com.example;
 
 /**
  * This is a class.
+ * @author Hayk
  */
 public class Greeter {
 
@@ -12,8 +13,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * @param String someone
+   * @return String formatted hello
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
